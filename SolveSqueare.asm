8d3154c62737956b46bf2cf6318d5c7660055eef
AnastasMIPT <54947616+AnastasMIPT@users.noreply.github.com>
2019-10-25 00:13:22 +0200

    a != 0 b = 0 c != 0

--- SolveSqueare.asm
+++ SolveSqueare.asm
@@ -12,8 +12,32 @@ JNE axnotnull
 CALL linenoeur
 
 :axnotnull
+
+PUSH 0
+PUSH bx
+JNE bnotnull
+PUSH -1
+PUUSH cx
+MUL
+POP cx
+
+PUUSH cx
+PUUSH ax
+DIV
+SQRT
+
+PUSH 1
+OUT
+OUT
+
+:bnotnull
+
 PUSH 2
 OUT
+
+
+
+
 
 RET
 
@@ -36,8 +60,10 @@ POP cx
 PUUSH cx
 PUUSH bx
 DIV
-SQRT
-
 OUT
+
 RET
+
+
+
 ENDING
