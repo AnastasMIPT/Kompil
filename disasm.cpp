163b05f5ac955f6fc96a36810ba7ed58a73e84f2
AnastasMIPT <54947616+AnastasMIPT@users.noreply.github.com>
2019-10-28 14:41:01 +0200

    Добавил return

--- disasm.cpp
+++ disasm.cpp
@@ -5,13 +5,28 @@
 //#include <stdlib.h>
 #include <sys\stat.h>
 
-#define DEF_CMD(name, num, mode, code)                 \
-                                                       \
-    if ((int) *ptr == CMD_##name) {                    \
-        fprintf (f_out, "%s\n", #name);                \
-                              \
-     }                                                 \
-     \
+#define DEF_CMD(name, num, mode, code)                  \
+                                                        \
+    if ((int) *ptr == CMD_##name) {                     \
+        fprintf (f_out, "%s ", #name);                  \
+        if (mode == NOARGUMENTS_T)                      \
+            fprintf (f_out, "");                     \
+        else if (mode == IMMED_T) {                     \
+            fprintf (f_out, "%d", *(int*) (ptr + 1)); \
+            ptr += sizeof (int);                        \
+        }                                               \
+        else if (mode == REGISTER_T) {                  \
+            fprintf (f_out, "%cx", *(ptr + 1));       \
+            ptr++;                                      \
+        }                                               \
+        else if (mode == LABEL_T) {                  \
+            fprintf (f_out, "%d", *(int*)(ptr + 1));       \
+            ptr += sizeof (int);                       \
+        }                                               \
+        fprintf (f_out, "\n");\
+                                                  \
+     }                                                  \
+                                                        \
 
 FILE* DISASM (FILE* f_in, FILE* f_out);
 
@@ -19,10 +34,11 @@ int main () {
 
     FILE* f_inp = fopen (AsmFilesPathO, "rb");
     FILE* f_outp = fopen (DisAsmFilesPathO, "w");
+
     assert (f_inp);
     assert (f_outp);
 
-    DISASM (f_inp, stdout);
+    DISASM (f_inp, f_outp);
 
 
     return 0;
@@ -41,12 +57,12 @@ FILE* DISASM (FILE* f_in, FILE* f_out) {
     char* buf = (char*) calloc (FIN.st_size, sizeof (char));
     fread (buf, sizeof (char), FIN.st_size, f_in);
     char* ptr = buf;
-    while ((int) *ptr != CMD_ENDING) {
+    while ((int) *ptr != CMD_ENDING ) {
         #include <AnastasLib\commands.h>
         ptr++;
     }
 
+    fprintf (f_out, "ENDING\n");
 
-
-
+    return f_out;
 }
